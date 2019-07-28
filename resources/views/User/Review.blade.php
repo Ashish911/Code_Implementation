eb2ba35c87c5b38151383be6e0dab6f40cf3b81b
Ashish911 <ashishdongol32@gmail.com>
2019-07-28 23:38:25 +0545

    smal

--- resources/views/User/Review.blade.php
+++ resources/views/User/Review.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.usersidebar')
 @section('content')
+
+    <style>
+        .card{
+            width: 600px;
+        }
+    </style>
+
     <div class="page-wrapper padding-t-a padding-b-a">
         <div class="wrapper wrapper--a">
-            <div class="card-4">
+                    <h2 class="title text-center font-weight-bold">Review </h2>
+            <div class="card offset-3">
                 <div class="card-body">
-                    <h2 class="title text-center font-weight-bold">Add Tattoo</h2>
                     <form method="post" name="edit" action="{{route('Review.store',Auth::user()->id)}}" enctype="multipart/form-data">
                         @csrf
                         <div class="row">
-                            <div class="col-lg-6 col-md-12">
-                                <div class="input-group">
+                                <div class="col-12">
                                     <label class="label">Comment</label>
-                                    <textarea class="input-style-aa" type="text" name="Comment" placeholder="review" required></textarea>
                                 </div>
-                            </div>
+                                <div class="col-12">
+                                    <textarea class="form-control" type="text" name="Comment" placeholder="review" required></textarea>
+                                </div>
                         </div>
-                        <div class="button--panel-2">
+                        <div class="button--panel-2 mt-2">
                             <input type="submit" class="btn btn-success" title="BecomeAnArtist" name="BecomeAnArtist" value="Submit"></input>
                         </div>
                     </form>
